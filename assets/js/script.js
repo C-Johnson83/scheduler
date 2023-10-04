d4163bca1c4b16dc32d76de4c2f361af632f160d
chris <chrisj21283@gmail.com>
2023-10-04 12:25:44 -0400

    css comment for header. variables made global

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
+var timeBlockId;
+var eventShow; 
+var saveBtn = $(".saveBtn");
+var eventSave;
+var currentDate;
+var currentHour = dayjs().format("HH");
 // TODO: Add code to display the current date in the header of the page.
 $(function () {
   // Display the current date in the header
@@ -9,51 +14,49 @@ $(function () {
   $("#currentDay").text(currentDate);
 });
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
- var saveBtn = $(".saveBtn");
- saveBtn.on("click", function () {
-  var timeBlockId = $(this).parent().attr("id");
-  var eventSave = $(this).siblings(".description").val();
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
+saveBtn.on("click", function () {
+  timeBlockId = $(this).parent().attr("id");
+  eventSave = $(this).siblings(".description").val();
   localStorage.setItem(timeBlockId, eventSave);
   console.log(timeBlockId);
   console.log(eventSave);
   console.log(localStorage);
- });
+});
 
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  $(".time-block").each(function () {
-    var timeBlockId = $(this).attr("id");
-    var currentHour = dayjs().format("HH");
-    // console.log(timeBlockId);
-    // console.log(currentHour);
-    if (timeBlockId > currentHour) {
-      $(this).addClass("future");
-    } else if (timeBlockId == currentHour) {
-      $(this).addClass("present");
-    } else {
-      $(this).addClass("past");
-    }
-  });
+// TODO: Add code to apply the past, present, or future class to each time
+// block by comparing the id to the current hour. HINTS: How can the id
+// attribute of each time-block be used to conditionally add or remove the
+// past, present, and future classes? How can Day.js be used to get the
+// current hour in 24-hour time?
+$(".time-block").each(function () {
+  timeBlockId = $(this).attr("id");
+  // console.log(timeBlockId);
+  // console.log(currentHour);
+  if (timeBlockId > currentHour) {
+    $(this).addClass("future");
+  } else if (timeBlockId == currentHour) {
+    $(this).addClass("present");
+  } else {
+    $(this).addClass("past");
+  }
+});
 
 
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
+//
 $(".description").each(function () {
-  var timeBlockId = $(this).parent().attr("id");
-  var eventShow = localStorage.getItem(timeBlockId);
+  timeBlockId = $(this).parent().attr("id");
+  eventShow = localStorage.getItem(timeBlockId);
   $(this).val(eventShow);
   console.log(timeBlockId);
   console.log(eventShow);
